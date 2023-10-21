6a6e8a94ba4805e333f8f772c4cd6680dbae4284
andreas <andreas@ros.land>
2023-10-20 20:20:13 -0500

    restructured home.nix for better readability

--- users/andreas/home.nix
+++ users/andreas/home.nix
@@ -13,52 +13,58 @@
     inherit pkgs;
   };
 in {
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+    tmux = tmuxConfig.programs.tmux;
+    lf = lfConfig.programs.lf;
+    git.enable = true;
+  };
 
-  home.username = "andreas";
-  home.homeDirectory = "/home/andreas";
-
-  home.stateVersion = "23.05";
-
-  programs.tmux = tmuxConfig.programs.tmux;
-  programs.lf = lfConfig.programs.lf;
-  programs.git.enable = true;
-
-  home.packages = with pkgs; [
-    deco
-    #browsers
-    google-chrome
-    firefox
-    #chat
-    mattermost-desktop
-    telegram-desktop
-    element-desktop
-    # Terminal essentials
-    alacritty
-    bat
-    eza
-    wtf
-    ripgrep
-    pass
-    neofetch
-    nix-tree
-    xsel
-    xclip
-    # SysAdmin
-    mtr
-    whois
-    magic-wormhole
-    #email
-    mutt-wizard
-    neomutt
-    isync
-    #editors
-    vscode
-    #other
-    vlc
-    mpv
-    gimp
-    inkscape
-    libreoffice
-  ];
+  home = {
+    username = "andreas";
+    homeDirectory = "/home/andreas";
+    stateVersion = "23.05";
+    packages = with pkgs; [
+      deco
+      #browsers
+      google-chrome
+      firefox
+      #chat
+      mattermost-desktop
+      telegram-desktop
+      element-desktop
+      # Terminal essentials
+      alacritty
+      bat
+      eza
+      wtf
+      ripgrep
+      pass
+      neofetch
+      nix-tree
+      xsel
+      xclip
+      unzip
+      file
+      # SysAdmin
+      sops
+      mtr
+      whois
+      magic-wormhole
+      xxd
+      tree
+      #email
+      mutt-wizard
+      neomutt
+      isync
+      #editors
+      vscode
+      #other
+      vlc
+      mpv
+      gimp
+      inkscape
+      libreoffice
+    ];
+  };
 }
